e8c3bb7631fca1dc21280ffe19b52453873509fe
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2024-12-24 17:29:12 +0200

    feat: project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 
-import App from './App.jsx'
-import './index.css'
+import Home from './pages/Home'
+import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </StrictMode>
 )
